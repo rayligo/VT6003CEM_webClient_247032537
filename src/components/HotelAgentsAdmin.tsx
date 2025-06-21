2e2a3eeff8184f84b5925206bec3f0542f061551
Li Yiu Yeung <rayli26go@gmail.com>
2025-06-22 05:28:49 +0800

    add Hotel Agents Admin function

---
+++ src/components/HotelAgentsAdmin.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { getCurrentUser } from "../services/auth.service";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import SearchUser from "./userSearch";
+
+const HotelAgentsAdmin: React.FC = () => {
+  const currentUser = getCurrentUser();
+  const navigate: NavigateFunction = useNavigate();
+
+  useEffect(() => {
+    if (!currentUser || currentUser.role !== "admin") {
+      navigate("/");
+    }
+  }, [currentUser, navigate]);
+
+  if (!currentUser || currentUser.role !== "admin") {
+    return null; // Prevent rendering if not an admin (handled by useEffect)
+  }
+
+  return (
+    <div style={{ marginLeft: "15px", marginBottom: "15px" }}>
+      <h2 style={{ color: "#135200" }}>Hotel Agents Administration</h2>
+      <SearchUser authbasic={`${localStorage.getItem("aToken")}`} />
+    </div>
+  );
+};
+
+export default HotelAgentsAdmin;
