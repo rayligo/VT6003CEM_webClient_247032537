567e60b2fb36d4648f818505c9b9a56e939eadbc
Li Yiu Yeung <rayli26go@gmail.com>
2025-06-22 05:42:29 +0800

    change  favpage

--- src/components/favpage.tsx
+++ src/components/favpage.tsx
@@ -1,14 +1,14 @@
-import 'antd/dist/reset.css';
-import '../App.css';
-import FavCard from './FavCard'
+import "antd/dist/reset.css";
+import "../App.css";
+import FavCard from "./FavCard";
 
 function FavPage() {
   return (
-    <> 
-    <h2 style={{ color: 'green' }}> Welcome to Hotel Agent</h2>     
-     
+    <>
+      <h2 style={{ color: "green" }}>favorites Hotel Agent</h2>
+
       <FavCard />
     </>
-  )
+  );
 }
 export default FavPage;
