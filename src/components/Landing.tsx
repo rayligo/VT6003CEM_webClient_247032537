379651cac7a873a1b5732b1cb8df3f88c76a23cd
Li Yiu Yeung <rayli26go@gmail.com>
2025-06-22 05:40:22 +0800

    change Landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,11 +1,11 @@
-import 'antd/dist/reset.css';
-import { Row, Col} from 'antd';
-import BriefArticle from './BriefArticle'
+import "antd/dist/reset.css";
+import { Row, Col } from "antd";
+import BriefArticle from "./BriefArticle";
 
 const Landing = () => {
   return (
     <>
-      <Row gutter={[16,16]} style={{marginLeft:"15px"}}>
+      <Row gutter={[16, 16]} style={{ marginLeft: "15px" }}>
         <Col span={8}>
           <BriefArticle />
         </Col>
@@ -17,7 +17,7 @@ const Landing = () => {
         </Col>
       </Row>
     </>
-  )
-}
+  );
+};
 
 export default Landing;
